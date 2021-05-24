d2653ef9dc044cc835777e347275edfea5c69c54
Van Linh Nguyen <linhvnguyen9@gmail.com>
2021-05-24 20:06:48 +0700

    Add linear search loop invariant analysis

--- src/main/java/searching/LinearSearch.kt
+++ src/main/java/searching/LinearSearch.kt
@@ -1,5 +1,17 @@
 package searching
 
+/**
+ * Linear search algorithm. Iterates over all elements in list until key is found.
+ *
+ * Loop invariant: a[0..i-1] is the list of elements that is not the search key
+ * Initialization: at the start of the loop i=0, so the list of elements which is not the search key is empty
+ * Maintenance: when our loop iterates over each element, it checks if that is the key or not, if so then the algorithm
+ * will return position of key, otherwise it'll move on
+ * Termination: when the algorithm finishes, either when we find the key in the list, or list doesn't contain key,
+ * a[0..i-1] still does not contain the search key
+ *
+ * @return -1 if key is not in the list, position of key otherwise
+ */
 fun List<Int>.linearSearch(key: Int): Int {
     this.forEachIndexed { index, value ->
         if (value == key)
--- src/test/java/searching/LinearSearchTest.kt
+++ src/test/java/searching/LinearSearchTest.kt
@@ -1,5 +1,27 @@
 package searching
 
+import org.junit.Test
 import org.junit.jupiter.api.Assertions.*
+import sorting.insertionSorted
+import kotlin.test.assertEquals
 
-internal class LinearSearchTest
+internal class LinearSearchTest {
+    @Test
+    fun `Linear search on key not in list`() {
+        val data = IntRange(1, 10).toList()
+
+        println("10 elements")
+
+        assertEquals(-1, data.linearSearch(11))
+    }
+
+    @Test
+    fun `Linear search on key in list`() {
+        val data = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        val unsortedList = data.shuffled()
+
+        println("10 elements")
+
+        assertEquals(4, data.linearSearch(5))
+    }
+}
